dcfcff9be22733b405ebd839b4598d1c2d30f9b6
Nicolai Bak <nba@it-minds.dk>
2022-10-03 13:18:59 +0200

    Redundant classes removed and class name changed

--- Pelican.Application/Deals/Commands/ChangeDealProperty/ChangeDealPropertyCommand.cs
+++ Pelican.Application/Deals/Commands/ChangeDealProperty/ChangeDealPropertyCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Pelican.Application.Abstractions.Messaging;
 using Pelican.Application.HubSpot.Commands.NewInstallation;
+using Pelican.Domain.Repositories;
 using Pelican.Domain.Shared;
 
 namespace Pelican.Application.Deals.Commands.ChangeDealProperty;
@@ -8,10 +9,14 @@ public sealed record ChangeDealPropertyCommand(
 	string PropertyName,
 	string PropertyValue) : ICommand;
 
-
 internal sealed class ChangeDealPropertyCommandHandler : ICommandHandler<NewInstallationCommand>
 {
-	public Task<Result> Handle(NewInstallationCommand request, CancellationToken cancellationToken)
+	readonly IDealRepository _dealRepository;
+	public ChangeDealPropertyCommandHandler(IDealRepository dealRepository)
+	{
+		_dealRepository = dealRepository;
+	}
+	public async Task<Result> Handle(NewInstallationCommand request, CancellationToken cancellationToken)
 	{
 
 		throw new NotImplementedException();

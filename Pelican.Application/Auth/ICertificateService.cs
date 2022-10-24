f6716ae9f22292940129379d57dbe2cdba7c47fc
AntonEsbjerg <202008162@post.au.dk>
2022-10-24 14:20:32 +0200

    Deleted No longer used interfaces for authorization

--- Pelican.Application/Auth/ICertificateService.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace Pelican.Application.Auth;
-public interface ICertificateService
-{
-	Task<X509Certificate2> GetGoogleCertificate(CancellationToken cancellationToken = default);
-}

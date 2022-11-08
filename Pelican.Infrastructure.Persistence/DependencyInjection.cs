ed41901cdba323dde39cff6c12fa3dd30f55a0e0
Nicolai Bak <nba@it-minds.dk>
2022-11-08 13:34:51 +0100

    secrets removed from appsettings

--- Pelican.Infrastructure.Persistence/DependencyInjection.cs
+++ Pelican.Infrastructure.Persistence/DependencyInjection.cs
@@ -1,19 +1,4 @@
-﻿using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
-using HotChocolate.Execution.Configuration;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Pelican.Application;
-using Pelican.Application.Common.Interfaces;
-using Pelican.Application.Common.Interfaces.DataLoaders;
-using Pelican.Application.Common.Interfaces.Repositories;
-using Pelican.Domain.Entities;
-using Pelican.Infrastructure.Persistence.DataLoader;
-using Pelican.Infrastructure.Persistence.Repositories;
-using Location = Pelican.Domain.Entities.Location;
+﻿using Location = Pelican.Domain.Entities.Location;
 
 namespace Pelican.Infrastructure.Persistence;
 public static class DependencyInjection
@@ -30,18 +15,23 @@ public static class DependencyInjection
 			AddLocalDb(services, configuration);
 		}
 		services.AddTransient<IUnitOfWork>(_ => new UnitOfWork(_.GetRequiredService<IDbContextFactory<PelicanContext>>().CreateDbContext()));
+
 		services.AddTransient<IPelicanBogusFaker, PelicanBogusFaker>();
+
 		services.AddTransient<DevelopmentSeeder>();
+
 		return services;
 	}
 	public static IRequestExecutorBuilder AddDataLoaders(this IRequestExecutorBuilder builder)
 	{
-		builder.AddDataLoader<IGenericDataLoader<AccountManager>, GenericDataLoader<AccountManager>>()
+		builder
+			.AddDataLoader<IGenericDataLoader<AccountManager>, GenericDataLoader<AccountManager>>()
 			.AddDataLoader<IGenericDataLoader<Client>, GenericDataLoader<Client>>()
 			.AddDataLoader<IGenericDataLoader<Contact>, GenericDataLoader<Contact>>()
 			.AddDataLoader<IGenericDataLoader<Deal>, GenericDataLoader<Deal>>()
 			.AddDataLoader<IGenericDataLoader<Location>, GenericDataLoader<Location>>()
 			.AddDataLoader<IGenericDataLoader<Supplier>, GenericDataLoader<Supplier>>();
+
 		return builder;
 	}
 
@@ -49,7 +39,10 @@ public static class DependencyInjection
 	{
 		if (app.Environment.IsDevelopment())
 		{
-			app.Services.GetRequiredService<DevelopmentSeeder>().SeedEntireDb(10);
+			app
+				.Services
+				.GetRequiredService<DevelopmentSeeder>()
+				.SeedEntireDb(10);
 		}
 
 		return app;

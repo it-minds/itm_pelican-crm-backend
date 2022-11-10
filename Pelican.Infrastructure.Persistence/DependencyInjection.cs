10c31338878d95271581b748270b36deca769750
AntonEsbjerg <202008162@post.au.dk>
2022-11-10 11:37:14 +0100

    Testing if connection string is empty

--- Pelican.Infrastructure.Persistence/DependencyInjection.cs
+++ Pelican.Infrastructure.Persistence/DependencyInjection.cs
@@ -67,7 +67,7 @@ public static class DependencyInjection
 		var kvUri = "https://" + keyVaultName + ".vault.azure.net";
 
 		var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
-		Console.WriteLine("CreatingContext");
+		Console.WriteLine(client.GetSecret(configuration["PelicanMsSQLSecret"]).Value.Value);
 		services.AddDbContextFactory<PelicanContext>(
 			o => o.UseSqlServer(client.GetSecret(configuration["PelicanMsSQLSecret"]).Value.Value,
 			b => b.MigrationsAssembly(typeof(PelicanContext).Assembly.FullName)));

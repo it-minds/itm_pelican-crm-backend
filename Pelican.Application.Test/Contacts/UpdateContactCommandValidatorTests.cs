ef0e7b0e56b4f48a34f2f2a6c053f0ef563c6306
Nicolai Bak <nba@it-minds.dk>
2022-11-15 09:33:26 +0100

    UpdateContact validator test added

---
+++ Pelican.Application.Test/Contacts/UpdateContactCommandValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentValidation.TestHelper;
+using Pelican.Application.Contacts.Commands.UpdateContact;
+using Xunit;
+
+namespace Pelican.Application.Test.Contacts;
+
+public class UpdateContactCommandValidatorTests
+{
+	private readonly UpdateContactCommandValidator _uut;
+
+	public UpdateContactCommandValidatorTests()
+	{
+		_uut = new();
+	}
+
+	[Fact]
+	public void UpdateContactommandValidator_EmptyStringOrDefaultValue_ReturnsError()
+	{
+		// Arrange
+		UpdateContactCommand command = new(
+			0,
+			0,
+			string.Empty,
+			string.Empty);
+
+		// Act
+		TestValidationResult<UpdateContactCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldHaveValidationErrorFor(command => command.ObjectId);
+		result.ShouldHaveValidationErrorFor(command => command.SupplierHubSpotId);
+		result.ShouldHaveValidationErrorFor(command => command.PropertyName);
+		result.ShouldHaveValidationErrorFor(command => command.PropertyValue);
+	}
+
+	[Fact]
+	public void UpdateContactommandValidator_NoEmptyStringsOrDefaultValues_ReturnsNoError()
+	{
+		// Arrange
+		UpdateContactCommand command = new(
+			1,
+			1,
+			"notEmpty",
+			"notEmpty");
+
+		// Act
+		TestValidationResult<UpdateContactCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldNotHaveValidationErrorFor(command => command.ObjectId);
+		result.ShouldNotHaveValidationErrorFor(command => command.SupplierHubSpotId);
+		result.ShouldNotHaveValidationErrorFor(command => command.PropertyName);
+		result.ShouldNotHaveValidationErrorFor(command => command.PropertyValue);
+	}
+
+}

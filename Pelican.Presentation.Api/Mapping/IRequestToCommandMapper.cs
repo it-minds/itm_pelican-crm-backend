964b400ead25cadcbe2b9eb7368cdede074e0eb7
Nicolai Bak <nba@it-minds.dk>
2022-11-17 14:46:02 +0100

    classes made public to enable swagger

--- Pelican.Presentation.Api/Mapping/IRequestToCommandMapper.cs
+++ Pelican.Presentation.Api/Mapping/IRequestToCommandMapper.cs
@@ -3,7 +3,7 @@ using Pelican.Presentation.Api.Contracts;
 
 namespace Pelican.Presentation.Api.Mapping;
 
-internal interface IRequestToCommandMapper
+public interface IRequestToCommandMapper
 {
 	IReadOnlyCollection<ICommand> ConvertToCommands(IReadOnlyCollection<WebHookRequest>? requests);
 }

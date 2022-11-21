e4f1000b6f89bbb0b5b76884c4d1fdee3e67d4eb
AntonEsbjerg <202008162@post.au.dk>
2022-11-21 16:13:53 +0100

    Added usings removed during merger

--- Pelican.Application/PelicanBogusFaker.cs
+++ Pelican.Application/PelicanBogusFaker.cs
@@ -1,4 +1,9 @@
-﻿namespace Pelican.Application;
+﻿using Bogus;
+using Pelican.Application.Common.Interfaces;
+using Pelican.Domain.Entities;
+using Pelican.Domain.Enums;
+
+namespace Pelican.Application;
 public class PelicanBogusFaker : IPelicanBogusFaker
 {
 	public PelicanBogusFaker() { }
@@ -39,9 +44,9 @@ public class PelicanBogusFaker : IPelicanBogusFaker
 		var faker = new Faker<Deal>().UseSeed(1339);
 		faker
 			.RuleFor(e => e.HubSpotId, f => f.Random.Guid().ToString())
-			.RuleFor(e => e.HubSpotOwnerId, f => f.PickRandom<AccountManager>(accountManagers).HubSpotId.OrNull(f))
-			.RuleFor(e => e.DealStatus, f => f.PickRandom<DealStatus>().ToString().OrNull(f))
-			.RuleFor(e => e.EndDate, f => f.Date.Past().OrNull(f))
+			.RuleFor(e => e.HubSpotOwnerId, f => f.PickRandom<AccountManager>(accountManagers).HubSpotId.OrNull(f, 0.0f))
+			.RuleFor(e => e.DealStatus, f => f.PickRandom<DealStatus>().ToString().OrNull(f, 0.0f))
+			.RuleFor(e => e.EndDate, f => f.Date.Past().OrNull(f, 0.0f))
 			.RuleFor(e => e.StartDate, f => f.Date.Future().OrNull(f, 0.0f))
 			.RuleFor(e => e.Id, f => f.Random.Guid())
 			.RuleFor(e => e.LastContactDate, f => f.Date.Past().OrNull(f, 0.0f))

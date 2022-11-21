3ff55bb4195f83e0509080d308fff542aa1f86cc
Nicolai Bak <nba@it-minds.dk>
2022-11-21 14:21:46 +0100

    context methods updated

--- Pelican.Infrastructure.Persistence/PelicanContext.cs
+++ Pelican.Infrastructure.Persistence/PelicanContext.cs
@@ -14,14 +14,8 @@ public class PelicanContext : DbContext, IPelicanContext
 		modelBuilder.ApplyConfigurationsFromAssembly(typeof(PelicanContext).Assembly);
 	}
 
-	public override DbSet<TEntity> Set<TEntity>() where TEntity : class
-	{
-		return base.Set<TEntity>();
-	}
+	public override DbSet<TEntity> Set<TEntity>() where TEntity : class => base.Set<TEntity>();
 
-	public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-	{
-		return base.SaveChangesAsync(cancellationToken);
-	}
+	public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) => base.SaveChangesAsync(cancellationToken);
 }
 

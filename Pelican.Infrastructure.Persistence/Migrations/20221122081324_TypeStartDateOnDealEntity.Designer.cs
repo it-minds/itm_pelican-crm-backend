cca6dfe0ccaa1abd655ac6274cb1ecada2e76212
AntonEsbjergIT-Minds <112855152+AntonEsbjergIT-Minds@users.noreply.github.com>
2022-11-22 10:00:50 +0100

    Feature/plcn 184 add start date to deal entity (#48)
    * Added StartDate And fixed Enddate for deal Entity
    
    * Added usings removed during merger
    
    * Added StartDate coloumn type to deal configuration.
    
    * Small fix
    
    * Added missing migration
    
    Signed-off-by: AntonEsbjergIT-Minds <112855152+AntonEsbjergIT-Minds@users.noreply.github.com>
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>

--- Pelican.Infrastructure.Persistence/Migrations/20221122081324_TypeStartDateOnDealEntity.Designer.cs
+++ Pelican.Infrastructure.Persistence/Migrations/20221122081324_TypeStartDateOnDealEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pelican.Infrastructure.Persistence;
 
@@ -11,9 +12,10 @@ using Pelican.Infrastructure.Persistence;
 namespace Pelican.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(PelicanContext))]
-    partial class PelicanContextModelSnapshot : ModelSnapshot
+    [Migration("20221122081324_TypeStartDateOnDealEntity")]
+    partial class TypeStartDateOnDealEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -281,7 +283,6 @@ namespace Pelican.Infrastructure.Persistence.Migrations
                         .HasColumnType("bigint");
 
                     b.Property<DateTime?>("StartDate")
-
                         .HasColumnType("Date");
 
                     b.HasKey("Id");

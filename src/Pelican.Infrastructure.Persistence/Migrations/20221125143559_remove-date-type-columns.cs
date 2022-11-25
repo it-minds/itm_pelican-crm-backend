3feeca3e8caf2a34df2095565a6da71b7f6f896b
Nicolai Bak <79926921+Nicolai-Bak@users.noreply.github.com>
2022-11-25 16:08:28 +0100

    feature/plcn-190-Add-src-and-test-folders (#52)
    * src and test folder added
    
    * date columns added
    
    Co-authored-by: Nicolai Bak <nba@it-minds.dk>

---
+++ src/Pelican.Infrastructure.Persistence/Migrations/20221125143559_remove-date-type-columns.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pelican.Infrastructure.Persistence.Migrations
+{
+    public partial class removedatetypecolumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Deals");
+
+            migrationBuilder.DropColumn(
+                name: "LastContactDate",
+                table: "Deals");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Deals");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Deals",
+                type: "Date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastContactDate",
+                table: "Deals",
+                type: "Date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Deals",
+                type: "Date",
+                nullable: true);
+        }
+    }
+}

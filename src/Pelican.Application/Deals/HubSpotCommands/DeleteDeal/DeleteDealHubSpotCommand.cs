8d4513d84306aa5a9811b92787c5341bfaaa9dfa
AntonEsbjerg <202008162@post.au.dk>
2022-11-29 16:56:35 +0100

    Added enpoint for update Pipedrive deal

--- src/Pelican.Application/Deals/HubSpotCommands/DeleteDeal/DeleteDealHubSpotCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using Pelican.Application.Abstractions.Messaging;
-
-namespace Pelican.Application.Deals.HubSpotCommands.DeleteDeal;
-
-public sealed record DeleteDealHubSpotCommand(
-	long ObjectId) : ICommand;

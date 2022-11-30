780baa05ea6d281bd29c06c7dcec740907ffada4
AntonEsbjerg <202008162@post.au.dk>
2022-11-30 08:53:47 +0100

    Added test for PipedriveController updatedeal pipedrive endpoint

--- test/Pelican.Presentation.Api.Test/Controllers/PipedriveControllerUnitTest.cs
+++ test/Pelican.Presentation.Api.Test/Controllers/PipedriveControllerUnitTest.cs
@@ -2,10 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Pelican.Application.Abstractions.Messaging;
-using Pelican.Application.Deals.PipedriveCommands.UpdateDeal;
+using Pelican.Application.Deals.Commands.UpdateDeal;
 using Pelican.Domain.Shared;
 using Pelican.Presentation.Api.Contracts.PipedriveWebHookRequests;
-using Pelican.Presentation.Api.Contracts.PipedriveWebHookRequests.UpdateDeal;
 using Pelican.Presentation.Api.Controllers;
 using Xunit;
 
@@ -32,7 +31,7 @@ public class PipedriveControllerUnitTest
 			.Setup(s => s.Send(It.IsAny<ICommand>(), It.IsAny<CancellationToken>()))
 			.ReturnsAsync(Result.Failure(new Error(errorCode, errorMessage)));
 
-		UpdateDealRequest dealResponse = new();
+		UpdateDealResponse dealResponse = new();
 
 		//Act
 		IActionResult result = await _uut.UpdateDeal(dealResponse);
@@ -47,57 +46,23 @@ public class PipedriveControllerUnitTest
 		Assert.IsType<BadRequestObjectResult>(result);
 	}
 
-	[Theory]
-	[InlineData("TestDescription", "TestName", 1, "TestContactDate", 1, 1, 1)]
-	public async void UpdateDeal_ReceivesCallAndReturnsSuccess_SuccessIsReturned(
-		string dealDescription,
-		string dealName,
-		int dealStatusId,
-		string lastContactDate,
-		int objectId,
-		int supplierPipedriveId,
-		int userId)
+	[Fact]
+	public async void UpdateDeal_ReceivesCallAndReturnsSuccess_SuccessIsReturned()
 	{
 		//Arrange
 		_senderMock
 			.Setup(s => s.Send(It.IsAny<ICommand>(), It.IsAny<CancellationToken>()))
 			.ReturnsAsync(Result.Success);
-		UpdateDealCurrentProperties updateDealCurrentProperties = new()
-		{
-			DealDescription = dealDescription,
-			DealName = dealName,
-			DealStatusId = dealStatusId,
-			LastContactDate = lastContactDate,
-		};
-		MetaProperties metaProperties = new()
-		{
-			ObjectId = objectId,
-			SupplierPipedriveId = supplierPipedriveId,
-			UserId = userId,
-		};
-		UpdateDealRequest dealRequest = new()
-		{
-			CurrentProperties = updateDealCurrentProperties,
-			MetaProperties = metaProperties,
-		};
-		UpdateDealPipedriveCommand expectedCommand = new(
-			supplierPipedriveId,
-			objectId,
-			userId,
-			dealStatusId,
-			dealDescription,
-			dealName,
-			lastContactDate,
-			null,
-			null);
+
+		UpdateDealResponse dealResponse = new();
 
 		//Act
-		IActionResult result = await _uut.UpdateDeal(dealRequest);
+		IActionResult result = await _uut.UpdateDeal(dealResponse);
 
 		//Assert
 		_senderMock.Verify(
 			s => s.Send(
-				expectedCommand,
+				It.IsAny<UpdateDealPipedriveCommand>(),
 				default),
 			Times.Once);
 

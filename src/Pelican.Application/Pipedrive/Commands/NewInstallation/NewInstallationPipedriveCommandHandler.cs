874ca4a471cea591a3406a7e341f1273a53ce566
AntonEsbjerg <202008162@post.au.dk>
2022-11-30 15:19:26 +0100

    Removed wrong assembly tag

--- src/Pelican.Application/Pipedrive/Commands/NewInstallation/NewInstallationPipedriveCommandHandler.cs
+++ src/Pelican.Application/Pipedrive/Commands/NewInstallation/NewInstallationPipedriveCommandHandler.cs
@@ -1,8 +1,6 @@
-﻿using System.Runtime.CompilerServices;
-using Pelican.Application.Abstractions.Messaging;
+﻿using Pelican.Application.Abstractions.Messaging;
 using Pelican.Domain.Shared;
 
-[assembly: InternalsVisibleTo("Pelican.Application.Test")]
 namespace Pelican.Application.Pipedrive.Commands.NewInstallation;
 
 internal sealed class NewInstallationCommandHandler : ICommandHandler<NewInstallationPipedriveCommand>

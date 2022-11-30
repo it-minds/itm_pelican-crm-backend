e6b34cfaba54bc3ad0af74f43e4a21ddabd04310
AntonEsbjerg <202008162@post.au.dk>
2022-11-30 16:18:38 +0100

    Test for AccountManager update endpoint and finished creating the endpoint.

---
+++ src/Pelican.Presentation.Api/Contracts/PipedriveWebHookRequests/AccountManager/UpdateAccountManager/UpdateAccountManagerRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Pelican.Presentation.Api.Contracts.PipedriveWebHookRequests.AccountManager.UpdateAccountManager;
+public record UpdateAccountManagerRequest
+{
+	[JsonPropertyName("current")]
+	public UpdateAccountManagerCurrentProperties CurrentProperties { get; set; } = new();
+
+	[JsonPropertyName("meta")]
+	public MetaProperties MetaProperties { get; set; } = new();
+}
--- test/Pelican.Presentation.Api.Test/Controllers/PipedriveControllerUnitTest.cs
+++ test/Pelican.Presentation.Api.Test/Controllers/PipedriveControllerUnitTest.cs
@@ -1,24 +1,4 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using Moq;
-using Pelican.Application.Abstractions.Messaging;
-using Pelican.Application.AccountManagers.PipedriveCommands.UpdateAccountManager;
-using Pelican.Application.Clients.PipedriveCommands.DeleteClient;
-using Pelican.Application.Clients.PipedriveCommands.UpdateClient;
-using Pelican.Application.Deals.PipedriveCommands.DeleteDeal;
-using Pelican.Application.Deals.PipedriveCommands.UpdateDeal;
-using Pelican.Application.Pipedrive.Commands.NewInstallation;
-using Pelican.Domain.Shared;
-using Pelican.Presentation.Api.Contracts.PipedriveWebHookRequests;
-using Pelican.Presentation.Api.Contracts.PipedriveWebHookRequests.AccountManager.Update;
-using Pelican.Presentation.Api.Contracts.PipedriveWebHookRequests.Client.Delete;
-using Pelican.Presentation.Api.Contracts.PipedriveWebHookRequests.Client.Update;
-using Pelican.Presentation.Api.Contracts.PipedriveWebHookRequests.Deal.Delete;
-using Pelican.Presentation.Api.Contracts.PipedriveWebHookRequests.Deal.Update;
-using Pelican.Presentation.Api.Controllers;
-using Xunit;
-
-namespace Pelican.Presentation.Api.Test.Controllers;
+﻿namespace Pelican.Presentation.Api.Test.Controllers;
 public class PipedriveControllerUnitTest
 {
 	private readonly PipedriveController _uut;
@@ -439,6 +419,89 @@ public class PipedriveControllerUnitTest
 				expectedCommand,
 				default),
 			Times.Once);
+
+		Assert.IsType<OkResult>(result);
+	}
+
+	[Theory]
+	[InlineData("0", "fail")]
+	public async void UpdateAccountManager_ReceivesCallAndReturnsFailure_FailureIsReturned(
+		string errorCode,
+		string errorMessage)
+	{
+		//Arrange
+		_senderMock
+			.Setup(s => s.Send(It.IsAny<ICommand>(), It.IsAny<CancellationToken>()))
+			.ReturnsAsync(Result.Failure(new Error(errorCode, errorMessage)));
+
+		UpdateAccountManagerRequest accountManagerRequest = new();
+
+		//Act
+		IActionResult result = await _uut.UpdateAccountManager(accountManagerRequest);
+
+		//Assert
+		_senderMock.Verify(
+			s => s.Send(
+				It.IsAny<UpdateAccountManagerPipedriveCommand>(),
+				default),
+			Times.Once);
+
+		Assert.IsType<BadRequestObjectResult>(result);
+	}
+
+	[Theory]
+	[InlineData("Anton Meldgaard Esbjerg", "TestMail", "TestPhoneNumber", "TestPictureUrl", 1, 1, 1)]
+	public async void UpdateAccountManager_ReceivesCallAndReturnsSuccess_SuccessIsReturned(
+		string testFullName,
+		string testEmail,
+		string testPhoneNumber,
+		string testPictureUrl,
+		int objectId,
+		int supplierPipedriveId,
+		int userId)
+	{
+		//Arrange
+		_senderMock
+			.Setup(s => s.Send(It.IsAny<ICommand>(), It.IsAny<CancellationToken>()))
+			.ReturnsAsync(Result.Success);
+		UpdateAccountManagerCurrentProperties updateAccountManagerCurrentProperties = new()
+		{
+			AccountManagerFullName = testFullName,
+			Email = testEmail,
+			PhoneNumber = testPhoneNumber,
+			PictureUrl = testPictureUrl,
+		};
+		MetaProperties metaProperties = new()
+		{
+			ObjectId = objectId,
+			SupplierPipedriveId = supplierPipedriveId,
+			UserId = userId,
+		};
+		UpdateAccountManagerRequest accountManagerRequest = new()
+		{
+			CurrentProperties = updateAccountManagerCurrentProperties,
+			MetaProperties = metaProperties,
+		};
+		UpdateAccountManagerPipedriveCommand expectedCommand = new(
+			supplierPipedriveId,
+			objectId,
+			userId,
+			"Anton Meldgaard",
+			"Esbjerg",
+			testPictureUrl,
+			testPhoneNumber,
+			testEmail,
+			null);
+
+		//Act
+		IActionResult result = await _uut.UpdateAccountManager(accountManagerRequest);
+
+		//Assert
+		_senderMock.Verify(
+			s => s.Send(
+					expectedCommand,
+					default),
+				Times.Once);
 
 		Assert.IsType<OkResult>(result);
 	}

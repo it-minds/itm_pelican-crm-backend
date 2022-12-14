776fccc714817baaab71702327ad8f4aba7d085a
Nicolai Bak <nba@it-minds.dk>
2022-12-14 15:48:23 +0100

    Updated tests

--- test/Pelican.Infrastructure.HubSpot.Test/Mapping/Contacts/ContactResponseToContactTests.cs
+++ test/Pelican.Infrastructure.HubSpot.Test/Mapping/Contacts/ContactResponseToContactTests.cs
@@ -1,4 +1,7 @@
-﻿using Bogus;
+﻿using System.Linq.Expressions;
+using Bogus;
+using Moq;
+using Pelican.Application.Abstractions.Data.Repositories;
 using Pelican.Domain;
 using Pelican.Domain.Entities;
 using Pelican.Infrastructure.HubSpot.Contracts.Responses.Common;
@@ -18,6 +21,8 @@ public class ContactResponseToContactTests
 	private const string JOBTITLE = "jobtitle";
 	private const string OWNERID = "ownerid";
 
+	private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+
 	private readonly ContactResponse response = new()
 	{
 		Properties = new()
@@ -33,14 +38,14 @@ public class ContactResponseToContactTests
 	};
 
 	[Fact]
-	public void ToContact_ResponseMissingHubSpotId_ThrowsException()
+	public async Task ToContact_ResponseMissingHubSpotId_ThrowsException()
 	{
 		/// Arrange
 		ContactResponse defaultResponse = new();
 		defaultResponse.Properties.HubSpotOwnerId = OWNERID;
 
 		/// Act
-		Exception result = Record.Exception(() => defaultResponse.ToContact());
+		Exception result = await Record.ExceptionAsync(() => defaultResponse.ToContact(_unitOfWorkMock.Object, default));
 
 		/// Assert
 		Assert.NotNull(result);
@@ -51,10 +56,10 @@ public class ContactResponseToContactTests
 	}
 
 	[Fact]
-	public void ToContact_WithoutAssociations_ReturnCorrectProperties()
+	public async Task ToContact_WithoutAssociations_ReturnCorrectPropertiesAndAssociations()
 	{
 		/// Act
-		Contact result = response.ToContact();
+		Contact result = await response.ToContact(_unitOfWorkMock.Object, default);
 
 		/// Assert
 		Assert.Equal(FIRSTNAME, result.FirstName);
@@ -65,62 +70,35 @@ public class ContactResponseToContactTests
 		Assert.Equal(JOBTITLE, result.JobTitle);
 		Assert.Equal(OWNERID, result.SourceOwnerId);
 		Assert.Equal(Sources.HubSpot, result.Source);
-	}
 
-	[Fact]
-	public void ToContact_WithoutAssociations_ReturnContactWithEmptyDealContacts()
-	{
-		/// Act
-		Contact result = response.ToContact();
-
-		/// Assert
 		Assert.Equal(0, result.DealContacts!.Count);
-	}
-
-	[Fact]
-	public void ToContact_WithoutAssociations_ReturnContactWithEmptyClientContacts()
-	{
-		/// Act
-		Contact result = response.ToContact();
-
-		/// Assert
 		Assert.Equal(0, result.ClientContacts!.Count);
 	}
 
 	[Fact]
-	public void ToContact_WithDefaultDeals_ReturnContactEmptyDealContacts()
+	public async Task ToContact_WithDefaultDeals_ReturnContactEmptyDealContactsAndEmptyClientContacts()
 	{
 		/// Arrange
 		response.Associations.Deals.AssociationList = new List<Association>()
 		{
 			new(),
 		};
-
-		/// Act
-		Contact result = response.ToContact();
-
-		/// Assert
-		Assert.Equal(0, result.DealContacts!.Count);
-	}
-
-	[Fact]
-	public void ToContact_WithDefaultCompanies_ReturnContactEmptyClientContacts()
-	{
-		/// Arrange
 		response.Associations.Companies.AssociationList = new List<Association>()
 		{
 			new(),
 		};
 
+
 		/// Act
-		Contact result = response.ToContact();
+		Contact result = await response.ToContact(_unitOfWorkMock.Object, default);
 
 		/// Assert
+		Assert.Equal(0, result.DealContacts!.Count);
 		Assert.Equal(0, result.ClientContacts!.Count);
 	}
 
 	[Fact]
-	public void ToContact_WithNotMatchingAssociations_ReturnContactEmptyDealContacts()
+	public async Task ToContact_WithNotMatchingAssociations_ReturnContactEmptyDealContacts()
 	{
 		/// Arrange
 		response.Associations.Deals.AssociationList = new List<Association>()
@@ -133,14 +111,14 @@ public class ContactResponseToContactTests
 		};
 
 		/// Act
-		Contact result = response.ToContact();
+		Contact result = await response.ToContact(_unitOfWorkMock.Object, default);
 
 		/// Assert
 		Assert.Equal(0, result.DealContacts!.Count);
 	}
 
 	[Fact]
-	public void ToContact_WithNotMatchingAssociations_ReturnContactEmptyClientContacts()
+	public async Task ToContact_WithNotMatchingAssociations_ReturnContactEmptyClientContacts()
 	{
 		/// Arrange
 		response.Associations.Companies.AssociationList = new List<Association>()
@@ -153,14 +131,14 @@ public class ContactResponseToContactTests
 		};
 
 		/// Act
-		Contact result = response.ToContact();
+		Contact result = await response.ToContact(_unitOfWorkMock.Object, default);
 
 		/// Assert
 		Assert.Equal(0, result.ClientContacts!.Count);
 	}
 
 	[Fact]
-	public void ToContact_WithMatchingAssociations_ReturnContactWithDeals()
+	public async Task ToContact_WithMatchingAssociations_ReturnContactWithDeals()
 	{
 		/// Arrange
 		response.Associations.Deals.AssociationList = new List<Association>()
@@ -172,8 +150,16 @@ public class ContactResponseToContactTests
 			},
 		};
 
+		_unitOfWorkMock
+			.Setup(u => u
+				.DealRepository
+				.FirstOrDefaultAsync(
+					It.IsAny<Expression<Func<Deal, bool>>>(),
+					It.IsAny<CancellationToken>()))
+			.ReturnsAsync(new Deal() { SourceId = "1" });
+
 		/// Act
-		Contact result = response.ToContact();
+		Contact result = await response.ToContact(_unitOfWorkMock.Object, default);
 
 		/// Assert
 		Assert.Equal(1, result.DealContacts!.Count);
@@ -183,82 +169,7 @@ public class ContactResponseToContactTests
 	}
 
 	[Fact]
-	public void ToContact_FirstNameStringTooLong_FirstNameShortenededAndAppendedWithThreeDots()
-	{
-		Faker faker = new();
-		response.Properties.FirstName = faker.Lorem.Letter(StringLengths.Name * 2);
-
-		/// Act
-		Contact result = response.ToContact();
-
-		/// Assert
-		Assert.Equal(StringLengths.Name, result.FirstName!.Length);
-		Assert.Equal("...", result.FirstName.Substring(StringLengths.Name - 3));
-		Assert.Equal(response.Properties.FirstName.Substring(0, StringLengths.Name - 3), result.FirstName.Substring(0, StringLengths.Email - 3));
-	}
-
-	[Fact]
-	public void ToContact_LastNameStringTooLong_LastNameShortenededAndAppendedWithThreeDots()
-	{
-		Faker faker = new();
-		response.Properties.LastName = faker.Lorem.Letter(StringLengths.Name * 2);
-
-		/// Act
-		Contact result = response.ToContact();
-
-		/// Assert
-		Assert.Equal(StringLengths.Name, result.LastName!.Length);
-		Assert.Equal("...", result.LastName.Substring(StringLengths.Name - 3));
-		Assert.Equal(response.Properties.LastName.Substring(0, StringLengths.Name - 3), result.LastName.Substring(0, StringLengths.Name - 3));
-	}
-
-	[Fact]
-	public void ToContact_EmailStringTooLong_EmailShortenededAndAppendedWithThreeDots()
-	{
-		Faker faker = new();
-		response.Properties.Email = faker.Lorem.Letter(StringLengths.Email * 2);
-
-		/// Act
-		Contact result = response.ToContact();
-
-		/// Assert
-		Assert.Equal(StringLengths.Email, result.Email!.Length);
-		Assert.Equal("...", result.Email.Substring(StringLengths.Email - 3));
-		Assert.Equal(response.Properties.Email.Substring(0, StringLengths.Email - 3), result.Email.Substring(0, StringLengths.Email - 3));
-	}
-
-	[Fact]
-	public void ToContact_PhoneStringTooLong_PhoneShortenededAndAppendedWithThreeDots()
-	{
-		Faker faker = new();
-		response.Properties.Phone = faker.Lorem.Letter(StringLengths.PhoneNumber * 2);
-
-		/// Act
-		Contact result = response.ToContact();
-
-		/// Assert
-		Assert.Equal(StringLengths.PhoneNumber, result.PhoneNumber!.Length);
-		Assert.Equal("...", result.PhoneNumber.Substring(StringLengths.PhoneNumber - 3));
-		Assert.Equal(response.Properties.Phone.Substring(0, StringLengths.PhoneNumber - 3), result.PhoneNumber.Substring(0, StringLengths.PhoneNumber - 3));
-	}
-
-	[Fact]
-	public void ToContact_JobTitleStringTooLong_JobTitleShortenededAndAppendedWithThreeDots()
-	{
-		Faker faker = new();
-		response.Properties.JobTitle = faker.Lorem.Letter(StringLengths.JobTitle * 2);
-
-		/// Act
-		Contact result = response.ToContact();
-
-		/// Assert
-		Assert.Equal(StringLengths.JobTitle, result.JobTitle!.Length);
-		Assert.Equal("...", result.JobTitle.Substring(StringLengths.JobTitle - 3));
-		Assert.Equal(response.Properties.JobTitle.Substring(0, StringLengths.JobTitle - 3), result.JobTitle.Substring(0, StringLengths.JobTitle - 3));
-	}
-
-	[Fact]
-	public void ToContact_WithMatchingAssociations_ReturnContactWithClientContacts()
+	public async Task ToContact_WithMatchingAssociations_ReturnContactWithClientContacts()
 	{
 		/// Arrange
 		response.Associations.Companies.AssociationList = new List<Association>()
@@ -270,8 +181,16 @@ public class ContactResponseToContactTests
 			},
 		};
 
+		_unitOfWorkMock
+			.Setup(u => u
+				.ClientRepository
+				.FirstOrDefaultAsync(
+					It.IsAny<Expression<Func<Client, bool>>>(),
+					It.IsAny<CancellationToken>()))
+			.ReturnsAsync(new Client() { SourceId = "1" });
+
 		/// Act
-		Contact result = response.ToContact();
+		Contact result = await response.ToContact(_unitOfWorkMock.Object, default);
 
 		/// Assert
 		Assert.Equal(1, result.ClientContacts!.Count);
--- test/Pelican.Infrastructure.HubSpot.Test/Mapping/Contacts/ContactsResponseToContactsTests.cs
+++ test/Pelican.Infrastructure.HubSpot.Test/Mapping/Contacts/ContactsResponseToContactsTests.cs
@@ -1,4 +1,6 @@
-﻿using Pelican.Domain;
+﻿using Moq;
+using Pelican.Application.Abstractions.Data.Repositories;
+using Pelican.Domain;
 using Pelican.Infrastructure.HubSpot.Contracts.Responses.Contacts;
 using Pelican.Infrastructure.HubSpot.Mapping.Contacts;
 using Xunit;
@@ -9,6 +11,8 @@ public class ContactsResponseToContactsTests
 {
 	const string ID = "id";
 	const string OWNERID = "ownerid";
+
+	private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
 
 	readonly ContactResponse response = new()
 	{
@@ -22,13 +26,13 @@ public class ContactsResponseToContactsTests
 	readonly ContactsResponse responses = new();
 
 	[Fact]
-	public void ToContacts_ArgResultsNull_ThrowException()
+	public async Task ToContacts_ArgResultsNull_ThrowExceptionAsync()
 	{
 		/// Arrange
 		responses.Results = null!;
 
 		/// Act
-		var result = Record.Exception(() => responses.ToContacts());
+		var result = await Record.ExceptionAsync(() => responses.ToContacts(_unitOfWorkMock.Object, default));
 
 		/// Assert
 		Assert.NotNull(result);
@@ -39,39 +43,39 @@ public class ContactsResponseToContactsTests
 	}
 
 	[Fact]
-	public void ToContacts_ArgResultsNotNull_ThrowNoException()
+	public async Task ToContacts_ArgResultsNotNull_ThrowNoExceptionAsync()
 	{
 		/// Arrange 
 		responses.Results = new List<ContactResponse>();
 
 		/// Act
-		var result = Record.Exception(() => responses.ToContacts());
+		var result = await Record.ExceptionAsync(() => responses.ToContacts(_unitOfWorkMock.Object, default));
 
 		/// Assert
 		Assert.Null(result);
 	}
 
 	[Fact]
-	public void ToContacts_ArgResultsNotNullNotEmpty_ThrowNoException()
+	public async Task ToContacts_ArgResultsNotNullNotEmpty_ThrowNoExceptionAsync()
 	{
 		/// Arrange 
 		responses.Results = new List<ContactResponse>() { response };
 
 		/// Act
-		var result = Record.Exception(() => responses.ToContacts());
+		var result = await Record.ExceptionAsync(() => responses.ToContacts(_unitOfWorkMock.Object, default));
 
 		/// Assert
 		Assert.Null(result);
 	}
 
 	[Fact]
-	public void ToContacts_SingleResponse_ReturnSingle()
+	public async void ToContacts_SingleResponse_ReturnSingle()
 	{
 		/// Arrange
 		responses.Results = new List<ContactResponse>() { response };
 
 		/// Act
-		var result = responses.ToContacts();
+		var result = await responses.ToContacts(_unitOfWorkMock.Object, default);
 
 		/// Assert
 		Assert.Equal(ID, result.First().SourceId);

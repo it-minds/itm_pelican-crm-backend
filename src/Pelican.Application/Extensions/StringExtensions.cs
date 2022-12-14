d60553902385e48db4e52e60ede2439fbf630f9e
Nicolai Bak <79926921+Nicolai-Bak@users.noreply.github.com>
2022-12-14 14:39:54 +0100

    Fix/plcn 280 check and fix hubspot updates on deals (#72)
    * service class added
    
    * tests added
    
    * corrections from pr
    
    * revert change
    
    * handler updated
    
    * Refactoring of mapping and handler
    
    * updates fixed
    
    * Deal tests updated
    
    * Tests updated
    
    * Deal tests updated
    
    * restsharp response tests added
    
    * update handler tests updated
    
    * more tests
    
    * restsharå name changes
    
    * comment removed
    
    * changes to accountmanger reverted
    
    * handling no owner on deal update
    
    * string extension moved to application project
    
    * test reorganized
    
    * Updated according to review
    
    * HubSpot Retry handling updated
    
    * test updated to hit scenario with deal update without accountmanager
    
    Co-authored-by: Nicolai Bak <nba@it-minds.dk>

---
+++ src/Pelican.Application/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Pelican.Application.Extensions;
+
+public static class StringExtensions
+{
+	public static long? ToUnixTimeMillisecondsOrNull(this string stringDate)
+		=> string.IsNullOrWhiteSpace(stringDate)
+		? null
+		: new DateTimeOffset(Convert.ToDateTime(stringDate)).ToUnixTimeMilliseconds();
+}

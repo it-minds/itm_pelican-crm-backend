0e54d7abfaec93e1ae6dad02d6c8ba9ac3551d3b
AntonEsbjerg <202008162@post.au.dk>
2022-12-15 09:33:04 +0100

    Re added tests to CompanyResponseToClientUnitTests

--- test/Pelican.Infrastructure.HubSpot.Test/Mapping/Clients/CompanyResponseToClientTests.cs
+++ test/Pelican.Infrastructure.HubSpot.Test/Mapping/Clients/CompanyResponseToClientTests.cs
@@ -1,9 +1,11 @@
-﻿using Bogus;
+﻿using System.Linq.Expressions;
+using Bogus;
 using Moq;
 using Pelican.Application.Abstractions.Data.Repositories;
 using Pelican.Domain;
 using Pelican.Domain.Entities;
 using Pelican.Infrastructure.HubSpot.Contracts.Responses.Clients;
+using Pelican.Infrastructure.HubSpot.Contracts.Responses.Common;
 using Pelican.Infrastructure.HubSpot.Mapping.Clients;
 using Xunit;
 
@@ -120,4 +122,195 @@ public class CompanyResponseToClientTests
 		Assert.Equal("...", result.Website.Substring(StringLengths.Url - 3));
 		Assert.Equal(response.Properties.Domain.Substring(0, StringLengths.Url - 3), result.Website.Substring(0, StringLengths.Url - 3));
 	}
+	[Fact]
+	public async void ToClient_WithoutAssociations_ReturnClientWithEmptyDeals()
+	{
+		/// Act
+		Client result = await response.ToClient(_unitOfWorkMock.Object, cancellationToken);
+
+		/// Assert
+		Assert.Equal(0, result.Deals!.Count);
+	}
+
+	[Fact]
+	public async void ToClient_WithoutAssociations_ReturnClientWithEmptyClientContacts()
+	{
+		/// Act
+		Client result = await response.ToClient(_unitOfWorkMock.Object, cancellationToken);
+
+		/// Assert
+		Assert.Equal(0, result.ClientContacts!.Count);
+	}
+
+	[Fact]
+	public async void ToClient_WithDefaultDealsAndContacts_ReturnClientEmptyDeals()
+	{
+		/// Arrange
+		response.Associations.Deals.AssociationList = new List<Association>()
+		{
+			new(),
+		};
+
+		response.Associations.Contacts.AssociationList = new List<Association>()
+		{
+			new(),
+		};
+
+		/// Act
+		Client result = await response.ToClient(_unitOfWorkMock.Object, cancellationToken);
+
+		/// Assert
+		Assert.Equal(0, result.Deals!.Count);
+	}
+
+	[Fact]
+	public async void ToClient_WithDefaultDealsAndContacts_ReturnClientEmptyClientContacts()
+	{
+		/// Arrange
+		response.Associations.Deals.AssociationList = new List<Association>()
+		{
+			new(),
+		};
+
+		response.Associations.Contacts.AssociationList = new List<Association>()
+		{
+			new(),
+		};
+
+		/// Act
+		Client result = await response.ToClient(_unitOfWorkMock.Object, cancellationToken);
+
+		/// Assert
+		Assert.Equal(0, result.ClientContacts!.Count);
+	}
+
+	[Fact]
+	public async void ToClient_WithNotMatchingAssociations_ReturnClientEmptyDeals()
+	{
+		/// Arrange
+		response.Associations.Deals.AssociationList = new List<Association>()
+		{
+			new()
+			{
+				Type = "not_matching",
+				Id = "2"
+			},
+		};
+
+		response.Associations.Contacts.AssociationList = new List<Association>()
+		{
+			new()
+			{
+				Type = "not_matching",
+				Id = "2"
+			},
+		};
+
+		/// Act
+		Client result = await response.ToClient(_unitOfWorkMock.Object, cancellationToken);
+
+		/// Assert
+		Assert.Equal(0, result.Deals!.Count);
+	}
+
+	[Fact]
+	public async void ToClient_WithNotMatchingAssociations_ReturnClientEmptyClientContacts()
+	{
+		/// Arrange
+		response.Associations.Deals.AssociationList = new List<Association>()
+		{
+			new()
+			{
+				Type = "not_matching",
+				Id = "2"
+			},
+		};
+
+		response.Associations.Contacts.AssociationList = new List<Association>()
+		{
+			new()
+			{
+				Type = "not_matching",
+				Id = "2"
+			},
+		};
+
+		/// Act
+		Client result = await response.ToClient(_unitOfWorkMock.Object, cancellationToken);
+
+		/// Assert
+		Assert.Equal(0, result.ClientContacts!.Count);
+	}
+
+	[Fact]
+	public async void ToClient_WithMatchingAssociations_ReturnClientWithDeals()
+	{
+		/// Arrange
+		Mock<Deal> dealMock = new();
+
+		response.Associations.Deals.AssociationList = new List<Association>()
+		{
+			new()
+			{
+				Type = "company_to_deal_unlabeled",
+				Id = "1"
+			},
+		};
+		dealMock.Object.SourceId = "1";
+		dealMock.Object.Source = Sources.HubSpot;
+		dealMock.Object.ClientId = Guid.Empty;
+
+		_unitOfWorkMock.Setup(
+			u => u.DealRepository
+				.FirstOrDefaultAsync(
+					It.IsAny<Expression<Func<Deal, bool>>>(),
+					It.IsAny<CancellationToken>()))
+			.ReturnsAsync(dealMock.Object);
+
+		/// Act
+		Client result = await response.ToClient(_unitOfWorkMock.Object, cancellationToken);
+
+		/// Assert
+		Assert.Equal(1, result.Deals!.Count);
+		Assert.Equal("1", result.Deals.First().SourceId);
+		Assert.Equal(result, result.Deals.First().Client);
+		Assert.Equal(result.Id, result.Deals.First().ClientId);
+	}
+
+	[Fact]
+	public async void ToClient_WithMatchingAssociations_ReturnClientWithClientContacts()
+	{
+		/// Arrange
+		Mock<Contact> contactMock = new();
+
+		response.Associations.Contacts.AssociationList = new List<Association>()
+		{
+			new()
+			{
+				Type = "company_to_contact_unlabeled",
+				Id = "1",
+			},
+		};
+		contactMock.Object.SourceId = "1";
+		contactMock.Object.Source = Sources.HubSpot;
+
+		_unitOfWorkMock.Setup(
+			u => u.ContactRepository
+				.FirstOrDefaultAsync(
+					It.IsAny<Expression<Func<Contact, bool>>>(),
+					It.IsAny<CancellationToken>()))
+			.ReturnsAsync(contactMock.Object);
+
+		/// Act
+		Client result = await response.ToClient(_unitOfWorkMock.Object, cancellationToken);
+
+		/// Assert
+		Assert.Equal(1, result.ClientContacts!.Count);
+		Assert.Equal("1", result.ClientContacts.First().SourceContactId);
+		Assert.Equal(result, result.ClientContacts.First().Client);
+		Assert.Equal(result.Id, result.ClientContacts.First().ClientId);
+		Assert.True(result.ClientContacts.First().IsActive);
+	}
+
+
 }

0ca57d0b1f969971644fa0c2c95df050dcc21d28
AntonEsbjergIT-Minds <112855152+AntonEsbjergIT-Minds@users.noreply.github.com>
2022-12-19 11:44:06 +0100

    Added IUnitOfWork to serviceBase (#83)
    * Added IUnitOfWork to serviceBase
    
    * Removed UnitOfWork as function argument on RefreshAccessTokenAsync
    
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>

--- test/Pelican.Application.Test/Contacts/Commands/UpdateContact/UpdateContactHubSpotCommandHandlerTests.cs
+++ test/Pelican.Application.Test/Contacts/Commands/UpdateContact/UpdateContactHubSpotCommandHandlerTests.cs
@@ -1,6 +1,4 @@
 ﻿using System.Linq.Expressions;
-using System.Threading;
-using Microsoft.EntityFrameworkCore;
 using Moq;
 using Pelican.Application.Abstractions.Data.Repositories;
 using Pelican.Application.Abstractions.HubSpot;

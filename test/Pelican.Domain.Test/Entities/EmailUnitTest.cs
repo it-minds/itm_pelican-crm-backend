fd4278c27ca36309fdfd26943ef44ee4554804e7
AntonEsbjerg <202008162@post.au.dk>
2022-12-20 15:23:06 +0100

    Added unittest for email entity

--- test/Pelican.Domain.Test/Entities/EmailUnitTest.cs
+++ test/Pelican.Domain.Test/Entities/EmailUnitTest.cs
@@ -101,6 +101,37 @@ public class EmailUnitTest
 	}
 
 	[Fact]
+	public void SetParagraph1_Paragraph1StringNotToLong_Paragraph1EqualToValueSet()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Paragraph);
+
+		// Act
+		_uut.Paragraph1 = propertyValue;
+
+		// Assert
+		Assert.Equal(StringLengths.Paragraph, _uut.Paragraph1!.Length);
+		Assert.Equal(propertyValue, _uut.Paragraph1);
+	}
+
+	[Fact]
+	public void SetParagraph1_Paragraph1StringToLong_Paragraph1ShortenedAndAppendedWithThreeDots()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Paragraph * 2);
+
+		// Act
+		_uut.Paragraph1 = propertyValue;
+
+		// Assert
+
+		Assert.Equal(StringLengths.Paragraph, _uut.Paragraph1!.Length);
+		Assert.Equal(propertyValue.Substring(0, StringLengths.Paragraph - 3) + "...", _uut.Paragraph1);
+	}
+
+	[Fact]
 	public void SetHeading2_Heading2StringNotToLong_Heading2EqualToValueSet()
 	{
 		// Arrange
@@ -111,7 +142,7 @@ public class EmailUnitTest
 		_uut.Heading2 = propertyValue;
 
 		// Assert
-		Assert.Equal(StringLengths.Heading, _uut.Heading1!.Length);
+		Assert.Equal(StringLengths.Heading, _uut.Heading2!.Length);
 		Assert.Equal(propertyValue, _uut.Heading2);
 	}
 
@@ -129,5 +160,129 @@ public class EmailUnitTest
 
 		Assert.Equal(StringLengths.Heading, _uut.Heading2!.Length);
 		Assert.Equal(propertyValue.Substring(0, StringLengths.Heading - 3) + "...", _uut.Heading2);
+	}
+
+	[Fact]
+	public void SetParagraph2_Paragraph2StringNotToLong_Paragraph2EqualToValueSet()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Paragraph);
+
+		// Act
+		_uut.Paragraph2 = propertyValue;
+
+		// Assert
+		Assert.Equal(StringLengths.Paragraph, _uut.Paragraph2!.Length);
+		Assert.Equal(propertyValue, _uut.Paragraph2);
+	}
+
+	[Fact]
+	public void SetParagraph2_Paragraph2StringToLong_Paragraph2ShortenedAndAppendedWithThreeDots()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Paragraph * 2);
+
+		// Act
+		_uut.Paragraph2 = propertyValue;
+
+		// Assert
+
+		Assert.Equal(StringLengths.Paragraph, _uut.Paragraph2!.Length);
+		Assert.Equal(propertyValue.Substring(0, StringLengths.Paragraph - 3) + "...", _uut.Paragraph2);
+	}
+
+	[Fact]
+	public void SetHeading3_Heading3StringNotToLong_Heading3EqualToValueSet()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Heading);
+
+		// Act
+		_uut.Heading3 = propertyValue;
+
+		// Assert
+		Assert.Equal(StringLengths.Heading, _uut.Heading3!.Length);
+		Assert.Equal(propertyValue, _uut.Heading3);
+	}
+
+	[Fact]
+	public void SetHeading3_Heading3StringToLong_Heading3ShortenedAndAppendedWithThreeDots()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Heading * 2);
+
+		// Act
+		_uut.Heading3 = propertyValue;
+
+		// Assert
+
+		Assert.Equal(StringLengths.Heading, _uut.Heading3!.Length);
+		Assert.Equal(propertyValue.Substring(0, StringLengths.Heading - 3) + "...", _uut.Heading3);
+	}
+
+	[Fact]
+	public void SetParagraph3_Paragraph3StringNotToLong_Paragraph3EqualToValueSet()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Paragraph);
+
+		// Act
+		_uut.Paragraph3 = propertyValue;
+
+		// Assert
+		Assert.Equal(StringLengths.Paragraph, _uut.Paragraph3!.Length);
+		Assert.Equal(propertyValue, _uut.Paragraph3);
+	}
+
+	[Fact]
+	public void SetParagraph3_Paragraph3StringToLong_Paragraph3ShortenedAndAppendedWithThreeDots()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Paragraph * 2);
+
+		// Act
+		_uut.Paragraph3 = propertyValue;
+
+		// Assert
+
+		Assert.Equal(StringLengths.Paragraph, _uut.Paragraph3!.Length);
+		Assert.Equal(propertyValue.Substring(0, StringLengths.Paragraph - 3) + "...", _uut.Paragraph3);
+	}
+
+	[Fact]
+	public void SetCtaButtonText_CtaButtonTextStringNotToLong_CtaButtonTextEqualToValueSet()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.CtaButtonText);
+
+		// Act
+		_uut.CtaButtonText = propertyValue;
+
+		// Assert
+		Assert.Equal(StringLengths.CtaButtonText, _uut.CtaButtonText!.Length);
+		Assert.Equal(propertyValue, _uut.CtaButtonText);
+	}
+
+	[Fact]
+	public void SetCtaButtonText_CtaButtonTextStringToLong_CtaButtonTextShortenedAndAppendedWithThreeDots()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.CtaButtonText * 2);
+
+		// Act
+		_uut.CtaButtonText = propertyValue;
+
+		// Assert
+
+		Assert.Equal(StringLengths.CtaButtonText, _uut.CtaButtonText!.Length);
+		Assert.Equal(propertyValue.Substring(0, StringLengths.CtaButtonText - 3) + "...", _uut.CtaButtonText);
 	}
 }

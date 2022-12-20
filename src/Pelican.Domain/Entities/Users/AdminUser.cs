84d307daa9cbcab7873dc8f06f760a87b4fb9244
AntonEsbjerg <202008162@post.au.dk>
2022-12-20 13:01:23 +0100

    Added AdminUser

---
+++ src/Pelican.Domain/Entities/Users/AdminUser.cs
@@ -0,0 +1,7 @@
+﻿namespace Pelican.Domain.Entities.Users;
+public class AdminUser : User
+{
+	public AdminUser() { }
+	//TODO re add this line when RoleEnum has been added
+	//public new RoleEnum{get;set;}=RoleEnum.Admin
+}

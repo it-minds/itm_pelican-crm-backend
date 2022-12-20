7d5b4b664ab4368e0d09aa0e5a932fa1af773f0d
AntonEsbjerg <202008162@post.au.dk>
2022-12-20 11:19:10 +0100

    Added tests for user entity

---
+++ test/Pelican.Domain.Test/Entities/UserUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+using Pelican.Domain.Entities;
+using Xunit;
+
+namespace Pelican.Domain.Test.Entities;
+public class UserUnitTest
+{
+	private readonly User _uut = new();
+
+	[Fact]
+	public void SetName_NameStringNotToLong_NameEqualToValueSet()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Name);
+
+		// Act
+		_uut.Name = propertyValue;
+
+		// Assert
+		Assert.Equal(StringLengths.Name, _uut.Name!.Length);
+		Assert.Equal(propertyValue, _uut.Name);
+	}
+}

7967799aab584c8cbb421556bd5a84525ddb09f2
AntonEsbjerg <202008162@post.au.dk>
2022-12-20 10:56:02 +0100

    Re added HUbSpotServiceFilter and database seeding.

--- src/Pelican.Presentation.Api/Controllers/HubSpotController.cs
+++ src/Pelican.Presentation.Api/Controllers/HubSpotController.cs
@@ -34,7 +34,7 @@ public sealed class HubSpotController : ApiController
 	}
 
 	[HttpPost]
-	//[ServiceFilter(typeof(HubSpotValidationFilter))]
+	[ServiceFilter(typeof(HubSpotValidationFilter))]
 	public async Task<IActionResult> Hook(
 		[FromBody] IReadOnlyCollection<HubSpotWebHookRequest> requests,
 		CancellationToken cancellationToken)

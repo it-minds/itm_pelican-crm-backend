84272848d14bfbfafa8e2947ca1c11759f2c4351
AntonEsbjerg <202008162@post.au.dk>
2022-12-21 15:04:49 +0100

    Created tests for LoginCommandValidator

---
+++ test/Pelican.Application.Test/Login/Commands/LoginCommandValidatorTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation.TestHelper;
+using Pelican.Application.Authentication.Login;
+using Xunit;
+
+namespace Pelican.Application.Test.Login.Commands;
+public class LoginCommandValidatorTests
+{
+	private readonly LoginCommandValidator _uut = new();
+
+	[Fact]
+	public void LoginCommandValidator_EmptyString_ReturnsError()
+	{
+		// Arrange
+		LoginCommand command = new(
+			string.Empty,
+			string.Empty);
+
+		// Act
+		TestValidationResult<LoginCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldHaveValidationErrorFor(command => command.Email);
+		result.ShouldHaveValidationErrorFor(command => command.Password);
+	}
+
+	[Fact]
+	public void LoginommandValidator_NoEmptyStrings_ReturnsNoError()
+	{
+		// Arrange
+		LoginCommand command = new(
+			"notEmpty",
+			"notEmpty");
+
+		// Act
+		TestValidationResult<LoginCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldNotHaveValidationErrorFor(command => command.Email);
+		result.ShouldNotHaveValidationErrorFor(command => command.Password);
+	}
+}

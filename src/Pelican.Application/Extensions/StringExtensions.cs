1f08a81ab328018724b08f02d76f22caa78bdb5f
AntonEsbjergIT-Minds <112855152+AntonEsbjergIT-Minds@users.noreply.github.com>
2022-12-21 13:22:52 +0100

    Feature/plcn 394 create string extension to check length (#91)
    * Created String extension for shortening string.
    
    * Using string extension to shorten string now.
    
    * Added null tests to entities
    
    * Readded usings
    
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>

--- src/Pelican.Application/Extensions/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pelican.Application.Extensions;
-
-public static class StringExtensions
-{
-	public static long? ToUnixTimeMillisecondsOrNull(this string stringDate)
-		=> string.IsNullOrWhiteSpace(stringDate)
-		? null
-		: new DateTimeOffset(Convert.ToDateTime(stringDate)).ToUnixTimeMilliseconds();
-}

c016d948e56f6ce97b16ba17ff760e20d5148aac
AntonEsbjerg <202008162@post.au.dk>
2022-12-21 14:57:41 +0100

    Created CheckAuth command.

---
+++ src/Pelican.Application/Authentication/CheckAuthCommand/CheckAuthCommand.cs
@@ -0,0 +1,6 @@
+﻿using Pelican.Application.Abstractions.Messaging;
+
+namespace Pelican.Application.Authentication.CheckAuthCommand;
+//TODO Add authenticated attribute when this is added.
+//[Authenticated]
+public sealed record CheckAuthCommand() : ICommand;

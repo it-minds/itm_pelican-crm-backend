cb365f53f373252f559449ae67aee68928ca0988
Nicolai Bak <79926921+Nicolai-Bak@users.noreply.github.com>
2022-12-21 14:47:53 +0100

    Fix/plcn 302 refactor hubspot new installation according to new mapping (#87)
    * handler updated
    
    * hubspot pagination limit set to 100
    
    * repo use in handler updated
    
    * tests updated
    
    * corrections from review
    
    * separated handler into helper functions
    
    Co-authored-by: Nicolai Bak <nba@it-minds.dk>

--- src/Pelican.Infrastructure.HubSpot/Mapping/Auth/AccessTokenResponseToSupplier.cs
+++ src/Pelican.Infrastructure.HubSpot/Mapping/Auth/AccessTokenResponseToSupplier.cs
@@ -7,10 +7,16 @@ namespace Pelican.Infrastructure.HubSpot.Mapping.Auth;
 internal static class AccessTokenResponseToSupplier
 {
 	internal static Supplier ToSupplier(this AccessTokenResponse response)
-		=> new()
+	{
+		var domainStrings = response.HubDomain.Split('.');
+		return new()
 		{
 			WebsiteUrl = response.HubDomain,
 			SourceId = response.HubId,
 			Source = Sources.HubSpot,
+			Name = domainStrings[0] == "www"
+				? domainStrings[1]
+				: domainStrings[0],
 		};
+	}
 }

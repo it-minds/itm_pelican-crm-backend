1355504873353ce6e238ca54b7cf4b9662205c56
AntonEsbjergIT-Minds <112855152+AntonEsbjergIT-Minds@users.noreply.github.com>
2022-12-22 13:46:31 +0100

    Created interfaces for Authorization-, CurrentUser-, Mail- and TokenService and also PasswordHasher (#95)
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>

---
+++ src/Pelican.Application/Abstractions/Authentication/IAuthorizationService.cs
@@ -0,0 +1,7 @@
+﻿using Pelican.Domain.Enums;
+
+namespace Pelican.Application.Abstractions.Authentication;
+public interface IAuthorizationService
+{
+	bool IsInRole(RoleEnum role);
+}
---
+++ src/Pelican.Application/Abstractions/Authentication/IPasswordHasher.cs
@@ -0,0 +1,6 @@
+﻿namespace Pelican.Application.Abstractions.Authentication;
+public interface IPasswordHasher
+{
+	string Hash(string password);
+	(bool Verified, bool NeedsUpgrade) Check(string hash, string password);
+}

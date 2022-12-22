81ebe16fccacb20f7b553e6f3f4fb990cca43a19
AntonEsbjergIT-Minds <112855152+AntonEsbjergIT-Minds@users.noreply.github.com>
2022-12-22 10:33:49 +0100

    Feature/plcn 363 create login command (#99)
    * Created Login Command
    
    * Created Validator for loginCommand
    
    * Created tests for LoginCommandValidator
    
    * Validator now checks if Email is correct format.
    
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>

---
+++ src/Pelican.Application/Authentication/Login/LoginCommand.cs
@@ -0,0 +1,4 @@
+﻿using Pelican.Application.Abstractions.Messaging;
+
+namespace Pelican.Application.Authentication.Login;
+public sealed record LoginCommand(string Email, string Password) : ICommand;

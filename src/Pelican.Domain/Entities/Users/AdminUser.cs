ae8369fe65e4fc48417bdd7eb82980d3264ed782
AntonEsbjergIT-Minds <112855152+AntonEsbjergIT-Minds@users.noreply.github.com>
2022-12-22 13:43:39 +0100

    Feature/plcn 319 create admin entity (#90)
    * User Entity created
    
    * Added tests for user entity
    
    * Added missing test to User Entity
    
    * Created an abstract user that standardUser can inherit from.
    
    * Added AdminUser
    
    * Added tests for admin entity
    
    * Readded outcommented lines.
    
    * Fixes after rebase
    
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>

---
+++ src/Pelican.Domain/Entities/Users/AdminUser.cs
@@ -0,0 +1,8 @@
+﻿using Pelican.Domain.Enums;
+
+namespace Pelican.Domain.Entities.Users;
+public class AdminUser : User
+{
+	public AdminUser() { }
+	public new RoleEnum Role { get; set; } = RoleEnum.Admin;
+}
---
+++ test/Pelican.Domain.Test/Entities/AdminUserUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using Pelican.Domain.Entities.Users;
+using Xunit;
+
+namespace Pelican.Domain.Test.Entities;
+public class AdminUserUnitTest
+{
+	private readonly AdminUser _uut = new();
+
+	[Fact]
+	public void SetName_NameStringNotToLong_NameEqualToValueSet()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Name);
+
+		// Act
+		_uut.Name = propertyValue;
+
+		// Assert
+		Assert.Equal(StringLengths.Name, _uut.Name!.Length);
+		Assert.Equal(propertyValue, _uut.Name);
+	}
+
+	[Fact]
+	public void SetName_NameStringToLong_NameShortenedAndAppendedWithThreeDots()
+	{
+		// Arrange
+		Faker faker = new();
+		string propertyValue = faker.Lorem.Letter(StringLengths.Name * 2);
+
+		// Act
+		_uut.Name = propertyValue;
+
+		// Assert
+
+		Assert.Equal(StringLengths.Name, _uut.Name!.Length);
+		Assert.Equal(propertyValue.Substring(0, StringLengths.Name - 3) + "...", _uut.Name);
+	}
+}

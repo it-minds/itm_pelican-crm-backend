f334fc636652418669fa688befca9a96023e18e8
AntonEsbjergIT-Minds <112855152+AntonEsbjergIT-Minds@users.noreply.github.com>
2022-12-22 10:33:49 +0100

    Feature/plcn 363 create login command (#99)
    * Created Login Command
    
    * Created Validator for loginCommand
    
    * Created tests for LoginCommandValidator
    
    * Validator now checks if Email is correct format.
    
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>

---
+++ src/Pelican.Application/Authentication/Login/LoginCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Pelican.Application.Authentication.Login;
+internal sealed class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+	public LoginCommandValidator()
+	{
+		RuleFor(command => command.Email)
+			.NotEmpty()
+			.EmailAddress();
+
+		RuleFor(command => command.Password)
+			.NotEmpty();
+	}
+}
---
+++ test/Pelican.Application.Test/Login/Commands/LoginCommandValidatorTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation.TestHelper;
+using Pelican.Application.Authentication.Login;
+using Xunit;
+
+namespace Pelican.Application.Test.Login.Commands;
+public class LoginCommandValidatorTests
+{
+	private readonly LoginCommandValidator _uut = new();
+
+	[Fact]
+	public void LoginCommandValidator_EmptyString_ReturnsError()
+	{
+		// Arrange
+		LoginCommand command = new(
+			string.Empty,
+			string.Empty);
+
+		// Act
+		TestValidationResult<LoginCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldHaveValidationErrorFor(command => command.Email);
+		result.ShouldHaveValidationErrorFor(command => command.Password);
+	}
+
+	[Fact]
+	public void LoginommandValidator_NoEmptyStringsButEmailIsNotInCorrectFormat_ReturnsError()
+	{
+		// Arrange
+		LoginCommand command = new(
+			"notEmpty",
+			"notEmpty");
+
+		// Act
+		TestValidationResult<LoginCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldHaveValidationErrorFor(command => command.Email);
+		result.ShouldNotHaveValidationErrorFor(command => command.Password);
+	}
+
+	[Fact]
+	public void LoginommandValidator_NoEmptyStringsAndEmailInCorrectFormat_ReturnsNoError()
+	{
+		// Arrange
+		LoginCommand command = new(
+			"a@a.com",
+			"notEmpty");
+
+		// Act
+		TestValidationResult<LoginCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldNotHaveValidationErrorFor(command => command.Email);
+		result.ShouldNotHaveValidationErrorFor(command => command.Password);
+	}
+}

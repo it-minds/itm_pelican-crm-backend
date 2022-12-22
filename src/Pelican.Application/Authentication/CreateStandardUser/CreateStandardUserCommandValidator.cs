ebb4fdf00f10453ef98e46a93091c7d9e4d27fd7
AntonEsbjerg <202008162@post.au.dk>
2022-12-22 11:21:33 +0100

    Create CreateStandardUserCommand and Validator with tests

---
+++ src/Pelican.Application/Authentication/CreateStandardUser/CreateStandardUserCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Pelican.Application.Authentication.CreateStandardUser;
+public class CreateStandardUserCommandValidator : AbstractValidator<CreateStandardUserCommand>
+{
+	public CreateStandardUserCommandValidator()
+	{
+		RuleFor(c => c.Email)
+			.NotEmpty()
+			.EmailAddress();
+		RuleFor(c => c.Password)
+			.NotEmpty().WithMessage("Your password cannot be empty")
+			.MinimumLength(8).WithMessage("Your password length must be a minimum of 8 characters")
+			.Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.")
+			.Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.")
+			.Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.");
+		RuleFor(c => c.Name)
+			.NotEmpty();
+	}
+}

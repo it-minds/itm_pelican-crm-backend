afe8bbc4da13a5e0925e7a44433bda011c01dfb4
AntonEsbjerg <202008162@post.au.dk>
2022-12-22 09:57:31 +0100

    Created Admin command, with validator and tests

---
+++ src/Pelican.Application/Authentication/CreateAdmin/CreateAdminCommand.cs
@@ -0,0 +1,4 @@
+﻿using Pelican.Application.Abstractions.Messaging;
+
+namespace Pelican.Application.Authentication.CreateAdmin;
+public sealed record CreateAdminCommand(string Name, string Email, string Password) : ICommand;

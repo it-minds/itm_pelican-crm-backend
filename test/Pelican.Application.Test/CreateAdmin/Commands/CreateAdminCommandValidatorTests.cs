15fac3610d4b99af0f9007efb1a029c9349f6096
AntonEsbjergIT-Minds <112855152+AntonEsbjergIT-Minds@users.noreply.github.com>
2022-12-27 09:46:54 +0100

    Feature/plcn 348 create create admin command and validator (#104)
    * Fix/plcn 280 check and fix hubspot updates on deals (#72)
    
    * service class added
    
    * tests added
    
    * corrections from pr
    
    * revert change
    
    * handler updated
    
    * Refactoring of mapping and handler
    
    * updates fixed
    
    * Deal tests updated
    
    * Tests updated
    
    * Deal tests updated
    
    * restsharp response tests added
    
    * update handler tests updated
    
    * more tests
    
    * restsharå name changes
    
    * comment removed
    
    * changes to accountmanger reverted
    
    * handling no owner on deal update
    
    * string extension moved to application project
    
    * test reorganized
    
    * Updated according to review
    
    * HubSpot Retry handling updated
    
    * test updated to hit scenario with deal update without accountmanager
    
    Co-authored-by: Nicolai Bak <nba@it-minds.dk>
    
    * Fix/plcn 283 check and fix hubspot updates on contacts (#74)
    
    * updated handler implementation
    
    * Updated tests
    
    * Removed setting id on create ClientContact
    
    * updated update dealcontacts implementation
    
    Co-authored-by: Nicolai Bak <nba@it-minds.dk>
    
    * Attempt to make ClientContact update correctly (#71)
    
    * Attempt to make ClientContact update correctly
    
    * Now Client also updates its deals.
    
    * Added test for UpdateDealsAsync
    
    * Readded HubSpotValidationFilter to HubSpotController and Database seeding.
    
    * Tried to align ClientUpdate with Deal update
    
    * Working on making sure The update Client HubSpot handler works as intended, new installation does not work atm.
    
    * UpdateCommandHandler for Client working tests to be updated
    
    * Fixed tests based on changes due to PR Review
    
    * Re added tests to CompanyResponseToClientUnitTests
    
    * Missing Client Tests
    
    * Fixes after rebase
    
    * Added test to client UpdatePropertiesFromClient
    
    * Added new tests to client
    
    * ClientContact now updates correctly.
    
    * Spacing made
    
    * Small whitespace fixes and readded servicefilter and seeding.
    
    * Changes based on PR
    
    * Wrote test for Client update deal
    
    * Full branch coverage for client should have been achieved.
    
    * Fixed test.
    
    * Whitespace
    
    * Updated UpdateClientHubSpotCommandHandler
    
    * Changes based on PR
    
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>
    
    * unused imports removed (#80)
    
    Co-authored-by: Nicolai Bak <nba@it-minds.dk>
    
    * Added IUnitOfWork to serviceBase (#83)
    
    * Added IUnitOfWork to serviceBase
    
    * Removed UnitOfWork as function argument on RefreshAccessTokenAsync
    
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>
    
    * Restored EFCore design and corrected dotnet-tools to be 6.0.10 (#85)
    
    * Restored EFCore design and corrected dotnet-tools to be 6.0.10
    
    * removed unused NugetPackage
    
    * Readded efcore design
    
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>
    
    * Hotfix/plcn 392 fix deal source owner id column not updating (#86)
    
    * Fixed update of SourceOwnerId column on Deal Entity
    
    * Re added HUbSpotServiceFilter and database seeding.
    
    * Readded missing using.
    
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>
    
    * Feature/plcn 394 create string extension to check length (#91)
    
    * Created String extension for shortening string.
    
    * Using string extension to shorten string now.
    
    * Added null tests to entities
    
    * Readded usings
    
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>
    
    * Feature/plcn 393 handle hubspot pagination when fetching data (#100)
    
    * pagination handling added
    
    * added pagination to ofter services
    
    * tests updated
    
    * tests updated
    
    Co-authored-by: Nicolai Bak <nba@it-minds.dk>
    
    * Created Login Command
    
    * Created Validator for loginCommand
    
    * Created tests for LoginCommandValidator
    
    * Created Admin command, with validator and tests
    
    * Fixes after rebase
    
    Co-authored-by: Nicolai Bak <79926921+Nicolai-Bak@users.noreply.github.com>
    Co-authored-by: Nicolai Bak <nba@it-minds.dk>
    Co-authored-by: AntonEsbjerg <202008162@post.au.dk>

---
+++ test/Pelican.Application.Test/CreateAdmin/Commands/CreateAdminCommandValidatorTests.cs
@@ -0,0 +1,116 @@
+﻿using FluentValidation.TestHelper;
+using Pelican.Application.Authentication.CreateAdmin;
+using Xunit;
+
+namespace Pelican.Application.Test.CreateAdmin.Commands;
+public class CreateAdminCommandValidatorTests
+{
+	private readonly CreateAdminCommandValidator _uut = new();
+
+	[Fact]
+	public void CreateAdminCommandValidator_EmptyString_ReturnsError()
+	{
+		// Arrange
+		CreateAdminCommand command = new(
+			string.Empty,
+			string.Empty,
+			string.Empty);
+
+		// Act
+		TestValidationResult<CreateAdminCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldHaveValidationErrorFor(command => command.Name);
+		result.ShouldHaveValidationErrorFor(command => command.Email);
+		result.ShouldHaveValidationErrorFor(command => command.Password).WithErrorMessage("Your password cannot be empty");
+
+	}
+
+	[Fact]
+	public void CreateAdminCommandValidator_NoEmptyStringsButEmailIsNotInCorrectFormatAndPasswordIsTooShort_ReturnsError()
+	{
+		// Arrange
+		CreateAdminCommand command = new(
+			"notEmpty",
+			"notEmpty",
+			"text");
+
+		// Act
+		TestValidationResult<CreateAdminCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldNotHaveValidationErrorFor(command => command.Name);
+		result.ShouldHaveValidationErrorFor(command => command.Email);
+		result.ShouldHaveValidationErrorFor(command => command.Password).WithErrorMessage("Your password length must be a minimum of 8 characters");
+	}
+
+	[Fact]
+	public void CreateAdminCommandValidator_NoEmptyStringsAndEmailInCorrectFormatAndPasswordContainsNoNumber_ReturnsNoError()
+	{
+		// Arrange
+		CreateAdminCommand command = new(
+			"notEmpty",
+			"a@a.com",
+			"notEmpty");
+
+		// Act
+		TestValidationResult<CreateAdminCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldNotHaveValidationErrorFor(command => command.Name);
+		result.ShouldNotHaveValidationErrorFor(command => command.Email);
+		result.ShouldHaveValidationErrorFor(command => command.Password).WithErrorMessage("Your password must contain at least one number.");
+	}
+
+	[Fact]
+	public void CreateAdminCommandValidator_NoEmptyStringsAndEmailInCorrectFormatAndPasswordContainsNoUpperCaseChar_ReturnsError()
+	{
+		// Arrange
+		CreateAdminCommand command = new(
+			"notEmpty",
+			"a@a.com",
+			"1newpassword");
+
+		// Act
+		TestValidationResult<CreateAdminCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldNotHaveValidationErrorFor(command => command.Name);
+		result.ShouldNotHaveValidationErrorFor(command => command.Email);
+		result.ShouldHaveValidationErrorFor(command => command.Password).WithErrorMessage("Your password must contain at least one uppercase letter.");
+	}
+	[Fact]
+	public void CreateAdminCommandValidator_NoEmptyStringsAndEmailInCorrectFormatAndPasswordContainsNoLowerCaseChar_ReturnsError()
+	{
+		// Arrange
+		CreateAdminCommand command = new(
+			"notEmpty",
+			"a@a.com",
+			"1NEWPASSWORD");
+
+		// Act
+		TestValidationResult<CreateAdminCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldNotHaveValidationErrorFor(command => command.Name);
+		result.ShouldNotHaveValidationErrorFor(command => command.Email);
+		result.ShouldHaveValidationErrorFor(command => command.Password).WithErrorMessage("Your password must contain at least one lowercase letter.");
+	}
+	[Fact]
+	public void CreateAdminCommandValidator_NoEmptyStringsAndEmailAndPasswordInCorrectFormat_ReturnsNoError()
+	{
+		// Arrange
+		CreateAdminCommand command = new(
+			"notEmpty",
+			"a@a.com",
+			"1NewPassword");
+
+		// Act
+		TestValidationResult<CreateAdminCommand> result = _uut.TestValidate(command);
+
+		// Assert
+		result.ShouldNotHaveValidationErrorFor(command => command.Name);
+		result.ShouldNotHaveValidationErrorFor(command => command.Email);
+		result.ShouldNotHaveValidationErrorFor(command => command.Password);
+	}
+}

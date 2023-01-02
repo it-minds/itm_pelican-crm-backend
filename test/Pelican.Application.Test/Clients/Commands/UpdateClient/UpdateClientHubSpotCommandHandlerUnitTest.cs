f820f0c5680fc6a40d87d8d346e7277d8950dd73
AntonEsbjerg <202008162@post.au.dk>
2023-01-02 10:50:54 +0100

    Removed duplicated test

--- test/Pelican.Application.Test/Clients/Commands/UpdateClient/UpdateClientHubSpotCommandHandlerUnitTest.cs
+++ test/Pelican.Application.Test/Clients/Commands/UpdateClient/UpdateClientHubSpotCommandHandlerUnitTest.cs
@@ -649,52 +649,6 @@ public class UpdateClientCommandHandlerTests
 		Assert.True(result.IsSuccess);
 	}
 
-	//[Theory]
-	//[InlineData(0, 0, 0, "num_associated_deals", "0")]
-	//public async void Handle_ClientFoundNumAssociatedDealsHubSpotAuthorizationServiceReturnsFailure_ReturnsFailure(
-	//	long objectId,
-	//	long portalId,
-	//	long updateTime,
-	//	string propertyName,
-	//	string propertyValue)
-	//{
-	//	//Arrange
-	//	UpdateClientHubSpotCommand command = new(objectId, portalId, updateTime, propertyName, propertyValue);
-
-	//	Mock<Client> clientMock = new();
-
-	//	_unitOfWorkMock.Setup(
-	//		unitOfWork => unitOfWork.ClientRepository.FindByCondition(
-	//			It.IsAny<Expression<Func<Client, bool>>>()))
-	//		.Returns(new List<Client> { clientMock.Object }.AsQueryable());
-
-	//	_hubSpotAuthorizationServiceMock.Setup(
-	//		h => h.RefreshAccessTokenFromSupplierHubSpotIdAsync(
-	//			It.IsAny<long>(),
-	//			It.IsAny<CancellationToken>()))
-	//		.ReturnsAsync(clientResult);
-
-	//	_unitOfWorkMock
-	//		.Setup(u => u
-	//			.ClientContactRepository
-	//			.AttachAsAdded(It.IsAny<IEnumerable<ClientContact>>()));
-
-	//	// Act
-	//	var result = await _uut.Handle(command, default);
-
-	//	//Assert
-	//	clientMock.Verify(
-	//		c => c.UpdatePropertiesFromClient(
-	//			clientResult),
-	//		Times.Once);
-
-	//	_unitOfWorkMock.Verify(
-	//		u => u.SaveAsync(default),
-	//		Times.Once);
-
-	//	Assert.True(result.IsSuccess);
-	//}
-
 	[Theory]
 	[InlineData(0, 0, 0, "num_associated_deals", "0")]
 	public async void Handle_ClientFoundNumAssociatedDealsHubSpotAuthorizationServiceReturnsFailure_ReturnsFailure(

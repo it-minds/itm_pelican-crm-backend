fe844d94385d05c7dd72b34acf3702ae2c4de8d2
AntonEsbjerg <202008162@post.au.dk>
2023-01-02 09:33:21 +0100

    Changed parameters on ResetPasswordCommand.

--- src/Pelican.Application/Authentication/ResetPassword/ResetPasswordCommand.cs
+++ src/Pelican.Application/Authentication/ResetPassword/ResetPasswordCommand.cs
@@ -1,4 +1,4 @@
 ﻿using Pelican.Application.Abstractions.Messaging;
 
 namespace Pelican.Application.Authentication.ResetPassword;
-public sealed record ResetPasswordCommand(string email) : ICommand<UserTokenDto>;
+public sealed record ResetPasswordCommand(string SSOToken, string NewPassword) : ICommand<UserTokenDto>;

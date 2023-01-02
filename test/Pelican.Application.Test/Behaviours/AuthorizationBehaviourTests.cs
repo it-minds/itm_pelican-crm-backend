31ba023ec443babef98f513dcc00f7056c5d9f5e
AntonEsbjerg <202008162@post.au.dk>
2023-01-02 13:47:44 +0100

    Attempted to test authorization behaviour but unable to

--- test/Pelican.Application.Test/Behaviours/AuthorizationBehaviourTests.cs
+++ test/Pelican.Application.Test/Behaviours/AuthorizationBehaviourTests.cs
@@ -3,17 +3,24 @@ using Moq;
 using Pelican.Application.Abstractions.Authentication;
 using Pelican.Application.Abstractions.Messaging;
 using Pelican.Application.Behaviours;
+using Pelican.Application.Security;
 using Pelican.Domain.Entities;
 using Xunit;
 
 namespace Pelican.Application.Test.Behaviours;
 public class AuthorizationBehaviourTests
 {
+
+	[Authorize(Role = Domain.Enums.RoleEnum.Standard)]
+	public record testQuery : IQuery<IQueryable<Contact>>
+	{
+	}
+
 	private Mock<ICurrentUserService> _userServiceMock = new();
 	private Mock<IAuthorizationService> _authorizationServiceMock = new();
-	private AuthorizationBehaviour<IRequest<Deal>, Deal> _uut;
-	private Mock<RequestHandlerDelegate<Deal>> _delegateMock = new();
-	Mock<IQuery<Deal>> _queryMock = new();
+	private AuthorizationBehaviour<IRequest<testQuery>, testQuery> _uut;
+	private Mock<RequestHandlerDelegate<testQuery>> _delegateMock = new();
+	Mock<IQuery<testQuery>> _queryMock = new();
 
 
 	public AuthorizationBehaviourTests()
@@ -21,20 +28,12 @@ public class AuthorizationBehaviourTests
 		_uut = new(_userServiceMock.Object, _authorizationServiceMock.Object);
 	}
 
-	public class testClass
-	{
-
-		public testClass()
-		{
-
-		}
-	}
 
 	[Fact]
 	public async void Test()
 	{
 		//Arrange
-
+		//_queryMock.Setup(x => x.GetType().).Returns(typeof(testQuery));
 
 		//Act
 		var result = await _uut.Handle(_queryMock.Object, _delegateMock.Object, default);

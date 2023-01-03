8b707233d2a179f7dd66546991b737e7d34a2340
AntonEsbjerg <202008162@post.au.dk>
2023-01-03 13:58:29 +0100

    Removed unnessary tag

--- src/Pelican.Application/Clients/Queries/GetClients/GetClientsQuery.cs
+++ src/Pelican.Application/Clients/Queries/GetClients/GetClientsQuery.cs
@@ -1,10 +1,8 @@
 ﻿using Pelican.Application.Abstractions.Messaging;
-using Pelican.Application.Security;
 using Pelican.Domain.Entities;
 
 namespace Pelican.Application.Clients.Queries.GetClients;
 //TODO Re-add these lines when the login has been implemented
 //[Authorize(Role = RoleEnum.Admin)]
 //[Authorize(Role = RoleEnum.Standard)]
-[Authenticated]
 public record GetClientsQuery() : IQuery<IQueryable<Client>>;

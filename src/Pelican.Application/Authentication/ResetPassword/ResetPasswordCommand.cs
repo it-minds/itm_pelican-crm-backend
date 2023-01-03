04c33402bfaf56c10cc7ccff80810f4a02efee9b
AntonEsbjerg <202008162@post.au.dk>
2023-01-03 10:35:08 +0100

    Added using after rebase.

--- src/Pelican.Application/Authentication/ResetPassword/ResetPasswordCommand.cs
+++ src/Pelican.Application/Authentication/ResetPassword/ResetPasswordCommand.cs
@@ -1,2 +1,4 @@
-﻿namespace Pelican.Application.Authentication.ResetPassword;
+﻿using Pelican.Application.Abstractions.Messaging;
+
+namespace Pelican.Application.Authentication.ResetPassword;
 public sealed record ResetPasswordCommand(string SSOToken, string NewPassword) : ICommand<UserTokenDto>;

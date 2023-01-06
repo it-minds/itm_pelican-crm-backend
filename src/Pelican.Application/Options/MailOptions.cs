2afe14c7686e943d9391cbfe178de37d423056c7
AntonEsbjerg <202008162@post.au.dk>
2023-01-06 14:33:53 +0100

    Create Mail-Options

---
+++ src/Pelican.Application/Options/MailOptions.cs
@@ -0,0 +1,14 @@
+﻿using Pelican.Domain.Entities;
+
+namespace Pelican.Application.Options;
+public class MailOptions
+{
+	public const string MailSettings = "MailSettings";
+	public string Mail { get; set; } = string.Empty;
+	public string DisplayName { get; set; } = string.Empty;
+	public string UserName { get; set; } = string.Empty;
+	public string Password { get; set; } = string.Empty;
+	public string Host { get; set; } = string.Empty;
+	public string baseUrl { get; set; } = string.Empty;
+	public List<Email> DefaultEmails { get; set; } = new List<Email>();
+}

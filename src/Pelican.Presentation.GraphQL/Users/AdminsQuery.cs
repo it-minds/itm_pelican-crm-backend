76c1874561941e9455586dfa7becea2da2a35dc9
AntonEsbjerg <202008162@post.au.dk>
2023-01-06 10:29:51 +0100

    Created GetAdminsQuery

---
+++ src/Pelican.Presentation.GraphQL/Users/AdminsQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Pelican.Application.Authentication;
+using Pelican.Application.Users.Queries.GetAdmins;
+
+namespace Pelican.Presentation.GraphQL.Users;
+[ExtendObjectType("Query")]
+public class AdminsQuery
+{
+	[UsePaging(IncludeTotalCount = true)]
+	[UseProjection]
+	[UseFiltering]
+	[UseSorting]
+	public async Task<IQueryable<UserDto>> GetAdminsAsync([Service] IMediator mediator, CancellationToken cancellationToken)
+	{
+		return await mediator.Send(new GetAdminsQuery(), cancellationToken);
+	}
+}
